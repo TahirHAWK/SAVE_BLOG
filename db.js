51600c6fdbc14031f1cc1976c4a06fed84067a66
TahirHSTU <45144696+TahirHSTU@users.noreply.github.com>
2021-07-01 21:25:51 +0600

    added note to db

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 
 const {axios, express, app, nodemailer, mongodb, connectionString} = require('./pages/dependencies')
 
-
+// while importing databases, do no import or export "Let db" part, as it causes error like 'undefined db' or something like that. so, in the dependencies folder, declare "Let db" but don't export it.
 
 
 let databaseConnection = mongodb.connect(connectionString, {useNewUrlParser: true}, function(err, client){
